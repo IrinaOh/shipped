63bf53f2f3e005299a42762a30733d3d61809368
Irina O'Hara <irina.v.ohara@gmail.com>
2016-12-20 23:15:44 -0700

    added functionality to create boats and jobs

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,17 +1,28 @@
 class JobsController < ApplicationController
   def index
+    @jobs = Job.all
+    @user = current_user
+    @boats = @user.boats
   end
 
   def new
+    @job = Job.new
   end
 
   def edit
   end
 
   def show
+    @job = Job.find(params[:id])
   end
 
   def create
+    @job = Job.new(description: params[:job][:description], destination: params[:job][:destination], origin: params[:job][:origin], cost: params[:job][:cost], container_amount: params[:job][:container_amount])
+    if @job.save
+      redirect_to jobs_path
+    else
+      render 'new'
+    end
   end
 
   def update
